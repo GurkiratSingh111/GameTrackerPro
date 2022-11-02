98b0fb69b7abbfe6d125d1fc25089a7b9e7109bb
Gahee Kim <gaheekimedu@gmail.com>
2022-11-01 18:28:56 -0700

    Add view achievements button to GameConfigActivity
    This button will lead to the Achievements Activity where user will be able to see the achievements for the game

--- app/src/main/java/ca/cmpt276/carbon/AchievementsActivity.java
+++ app/src/main/java/ca/cmpt276/carbon/AchievementsActivity.java
@@ -2,7 +2,11 @@ package ca.cmpt276.carbon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+
+import ca.cmpt276.carbon.model.AchievementLevel;
 
 public class AchievementsActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class AchievementsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievements);
+    }
+
+    public static Intent makeLaunchIntent(Context c) {
+        Intent intent = new Intent(c, AchievementsActivity.class);
+        return intent;
     }
 }
